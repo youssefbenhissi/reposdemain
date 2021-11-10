fde98e7e40d30c624f1f0a0c3d938d650e5e3afb
Youssef Benhissi <youssef.benhissi@esprit.tn>
2021-11-10 01:25:00 +0100

    waa

--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const userController = require("../controllers/user.controller")
 
-
-const multer = require("multer");
-const path = require("path");
-
-const storageData = multer.diskStorage({
-    destination: (req, file, clb) => {
-        clb(null, './public/images/');
-    },
-    filename: (req, file, cb) => {
-        const newFileName = new Date().getTime().toString() + path.extname(file.originalname);
-        cb(null, newFileName);
-    }
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
 });
-
-const upload = multer({ storage : storageData })
-
-/**
- * @Path /users
- **/
-router.route("/create")
-  .get(userController.showCreateForm)
-  .post(upload.single("avatar") ,userController.createUser)
-
-router.get("/", userController.getListUsers)
-router.get("/:_id", userController.getUserById)
-router.get("/delete/:id", userController.deleteUser)
 
 module.exports = router;
